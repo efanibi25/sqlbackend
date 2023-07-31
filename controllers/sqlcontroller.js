a8be435fb18aaf2a9adc68eb947eb1780a1ca469
emmanuel.fanibi <58971180+efanibi25@users.noreply.github.com>
2023-07-31 14:57:58 -0500

    add generate.js

--- controllers/sqlcontroller.js
+++ controllers/sqlcontroller.js
@@ -1,9 +1,9 @@
 //Takes a user input
 
-const { sendChatGPT } = require("../tools/chatgpt")
+const { sendChatGPT } = require("../tools/generate")
 
 // get chatgpt to respond with the a array of the sql query
-require("../tools/chatgpt")
+require("../tools/generate")
 
 const sqlconvert=async function (text){
    return await sendChatGPT(text)
